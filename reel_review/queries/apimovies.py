8a073dd792b1f327ca5dc3605cc55abe343d21de
Jeremiah Rigby <jrigby97@gmail.com>
2023-02-16 15:00:56 -0700

    returning only wanted data

--- reel_review/queries/apimovies.py
+++ reel_review/queries/apimovies.py
@@ -3,15 +3,22 @@ import os
 from keys import MOVIE_KEY
 
 API_KEY = MOVIE_KEY
-print(API_KEY)
+# print(API_KEY)
 
 
 class MovieQueries:
     def get_movie_by_name(self, title: str):
+        returned_movies = []
         res = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&language=en-US&query={title}&page=1&include_adult=false')
-        print(res)
         data = res.json()
-        return data
+        for movie in data["results"]:
+            temp = {}
+            temp["movie_id"]= movie["id"]
+            temp["title"] = movie["title"]
+            temp["poster_path"]= movie["poster_path"]
+            temp["vote_average"]= movie["vote_average"]
+            returned_movies.append(temp)
+        return returned_movies
 
 movie = MovieQueries()
-print(movie.get_movie_by_name(title = 'Shawshank'))
+movie.get_movie_by_name(title = 'shawshank')

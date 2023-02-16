342171605a12ae23b4126416752ed3869d3fa014
Blackfeng <blackfengsm@gmail.com>
2023-02-16 14:04:36 -0600

    api key

--- reel_review/queries/apimovies.py
+++ reel_review/queries/apimovies.py
@@ -1,8 +1,14 @@
 import requests
-from keys import MOVIE_KEY
+import os
+
+api_key = os.environ.get("MOVIE_KEY")
+print(api_key, '**************')
 
 class MovieQueries:
     def get_movie_by_name(self, title: str):
-        res = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={MOVIE_KEY}&language=en-US&query={SearchIn}&page=1&include_adult=false')
+        res = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US&query={title}&page=1&include_adult=false')
         data = res.json()
         return data
+
+movie = MovieQueries()
+print(movie.get_movie_by_name(title = 'Shawshank'))

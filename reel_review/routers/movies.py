71ba92e245e63d313e0ddcb0dd92a286cea8de0e
Blackfeng <blackfengsm@gmail.com>
2023-02-21 14:48:10 -0600

    changes

--- reel_review/routers/movies.py
+++ reel_review/routers/movies.py
@@ -9,14 +9,15 @@ router = APIRouter()
 @router.get('/api/movies/search', response_model=List[SearchOut])
 def get_movie_list(
     title: str,
-    # repo: SearchRepository = Depends()
+    repo: SearchRepository = Depends()
 ):
-    repo = SearchRepository()
+    # repo = SearchRepository()
     return repo.search(title)
 
 @router.get("/api/movie", response_model=MovieDetail)
 def movie_detail(
     id: int
+    # repo: SearchRepository = Depends()
 ):
     repo = SearchRepository()
     return repo.detail(id)

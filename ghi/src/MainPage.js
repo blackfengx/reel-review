9154ccf5f9df002c2a512c1f90fee1b053d66795
Blackfeng <blackfengsm@gmail.com>
2023-02-22 17:17:02 -0600

    front end forms

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import TrendingMovies from './components/TrendingMovies'
+import SignUp from './components/SignUp'
+
 
 export default function MainPage() {
 
@@ -7,7 +9,8 @@ export default function MainPage() {
   return (
     <>
     <h1>MainPage</h1>
-    <TrendingMovies/>
+    {/* <TrendingMovies/> */}
+    <SignUp/>
     </>
   )
 }

4c449b8d1b99a455091ff23e57d128ce505fd7af
Blackfeng <blackfengsm@gmail.com>
2023-02-23 16:21:32 -0600

    LOL!!!!

--- ghi/src/components/Login.js
+++ ghi/src/components/Login.js
@@ -1,68 +1,78 @@
-import React, { useState } from "react";
-import { useToken } from "./auth";
+// import { useState } from "react";
+// import { useToken } from "./auth";
+// import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const { setToken } = useToken();
+// function LoginComponent() {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [token, login] = useToken();
+//   const navigate = useNavigate();
 
-  const url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/token`;
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const resp = await login(username, password);
+//       if (resp !== false) {
+//         setUsername("");
+//         setPassword("");
+//         navigate("/");
+//         window.location.reload(false);
+//       } else {
+//         alert("Invalid email or password");
+//       }
+//       console.log(token);
+//     } catch (error) {
+//       console.log(error);
+//       console.log("Wrong email or password!");
+//     }
+//   };
 
-  const form = new FormData();
-  form.append("username", username);
-  form.append("password", password);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await fetch(url, {
-      method: "post",
-      credentials: "include",
-      body: form,
-    });
-    if (response.ok) {
-      const tokenUrl = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/token`;
-
-      try {
-        const response = await fetch(tokenUrl, {
-          credentials: "include",
-        });
-        if (response.ok) {
-          const data = await response.json();
-          const token = data.access_token;
-          setToken(token);
-          // DO SOMETHING WITH THE TOKEN SO YOU CAN USE IT
-          // IN REQUESTS TO YOUR NON-ACCOUNTS SERVICES
-        }
-      } catch (e) {}
-    } else {
-      let error = await response.json();
-      // DO SOMETHING WITH THE ERROR, IF YOU WANT
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-
-export default Login;
+//   return (
+//     <div className="positioning">
+//       <div className="container2">
+//         <form onSubmit={handleSubmit}>
+//           <div className="mb-3">
+//             <label
+//               htmlFor="username"
+//               className="form-label"
+//               style={{ color: "#E23E57" }}
+//             >
+//               Username
+//             </label>
+//             <input
+//               onChange={(e) => setUsername(e.target.value)}
+//               value={username}
+//               type="username"
+//               className="form-control"
+//               id="username"
+//             />
+//           </div>
+//           <div className="mb-3">
+//             <label
+//               htmlFor="password"
+//               className="form-label"
+//               style={{ color: "#E23E57" }}
+//             >
+//               Password
+//             </label>
+//             <input
+//               onChange={(e) => setPassword(e.target.value)}
+//               value={password}
+//               type="password"
+//               className="form-control"
+//               id="password"
+//             />
+//           </div>
+//           <button
+//             className="btn btn-outline-light my-2 my-sm-0"
+//             type="submit"
+//             style={{ color: "#E23E57" }}
+//           >
+//             Submit
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+// export default LoginComponent;
--- ghi/src/components/auth.js
+++ ghi/src/components/auth.js
@@ -1,174 +1,128 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { createContext, useContext, useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// let internalToken = null;
 
-let internalToken = null;
+// // Get Token
 
-export function getToken() {
-  return internalToken;
-}
+// export function getToken() {
+//   return internalToken;
+// }
 
-export async function getTokenInternal() {
-  const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-  try {
-    const response = await fetch(url, {
-      credentials: "include",
-    });
-    if (response.ok) {
-      const data = await response.json();
-      internalToken = data.access_token;
-      return internalToken;
-    }
-  } catch (e) {}
-  return false;
-}
 
-function handleErrorMessage(error) {
-  if ("error" in error) {
-    error = error.error;
-    try {
-      error = JSON.parse(error);
-      if ("__all__" in error) {
-        error = error.__all__;
-      }
-    } catch {}
-  }
-  if (Array.isArray(error)) {
-    error = error.join("<br>");
-  } else if (typeof error === "object") {
-    error = Object.entries(error).reduce(
-      (acc, x) => `${acc}<br>${x[0]}: ${x[1]}`,
-      ""
-    );
-  }
-  return error;
-}
+// export async function getTokenInternal() {
+//   const url = `${process.env.REACT_APP_TUNEWORLD_API_HOST}/token`;
+//   try {
+//     const response = await fetch(url, {
+//       credentials: "include",
+//     });
+//     if (response.ok) {
+//       const data = await response.json();
+//       internalToken = data.access_token;
+//       return internalToken;
+//     }
+//   } catch (e) {}
+//   return false;
+// }
 
-export const AuthContext = createContext({
-  token: null,
-  setToken: () => null,
-});
 
-export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+// export const AuthContext = createContext({
+//   token: null,
+//   setToken: () => null,
+// });
 
-  return (
-    <AuthContext.Provider value={{ token, setToken }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+// export const AuthProvider = ({ children }) => {
+//   const [token, setToken] = useState(null);
 
-export const useAuthContext = () => useContext(AuthContext);
+//   return (
+//     <AuthContext.Provider value={{ token, setToken }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
 
-export function useToken() {
-  const { token, setToken } = useAuthContext();
-  const navigate = useNavigate();
+// export const useAuthContext = () => useContext(AuthContext);
 
-  useEffect(() => {
-    async function fetchToken() {
-      const token = await getTokenInternal();
-      setToken(token);
-    }
-    if (!token) {
-      fetchToken();
-    }
-  }, [setToken, token]);
+// export function useToken() {
+//   const { token, setToken } = useAuthContext();
+//   const navigate = useNavigate();
 
-  async function logout() {
-    if (token) {
-      const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-      await fetch(url, { method: "delete", credentials: "include" });
-      internalToken = null;
-      setToken(null);
-      navigate("/");
-    }
-  }
+//   useEffect(() => {
+//     async function fetchToken() {
+//       const token = await getTokenInternal();
+//       setToken(token);
+//     }
+//     if (!token) {
+//       fetchToken();
+//     }
+//   }, [setToken, token]);
 
-  async function login(username, password) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/token`;
-    const form = new FormData();
-    form.append("username", username);
-    form.append("password", password);
-    const response = await fetch(url, {
-      method: "post",
-      credentials: "include",
-      body: form,
-    });
-    if (response.ok) {
-      const token = await getTokenInternal();
-      setToken(token);
-      return;
-    }
-    let error = await response.json();
-    return handleErrorMessage(error);
-  }
+//   async function logout() {
+//     if (token) {
+//       const url = `${process.env.REACT_APP_TUNEWORLD_API_HOST}/token`;
+//       await fetch(url, { method: "delete", credentials: "include" });
+//       internalToken = null;
+//       setToken(null);
+//       navigate("/");
+//     }
+//   }
 
-  async function signup(username, password, email, firstName, lastName) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/sign_up`;
-    const response = await fetch(url, {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        password,
-        email,
-        first_name: firstName,
-        last_name: lastName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      await login(username, password);
-    }
-    return false;
-  }
+//   async function login(email, password) {
+//     const url = `${process.env.REACT_APP_TUNEWORLD_API_HOST}/token`;
+//     const form = new FormData();
+//     form.append("username", email.toLowerCase());
+//     form.append("password", password);
+//     const response = await fetch(url, {
+//       method: "post",
+//       credentials: "include",
+//       body: form,
+//     });
+//     if (response.ok) {
+//       const token = await getTokenInternal();
+//       setToken(token);
+//       return;
+//     }
+//     await response.json();
+//     return false;
+//   }
 
-  async function update(username, password, email, firstName, lastName) {
-    const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/accounts`;
-    const response = await fetch(url, {
-      method: "patch",
-      body: JSON.stringify({
-        username,
-        password,
-        email,
-        first_name: firstName,
-        last_name: lastName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      await login(username, password);
-    }
-    return false;
-  }
+//   async function signup(password, email, full_name) {
+//     const url = `${process.env.REACT_APP_TUNEWORLD_API_HOST}/token`;
+//     const response = await fetch(url, {
+//       method: "post",
+//       body: JSON.stringify({
+//         password,
+//         email,
+//         full_name: full_name,
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     if (response.ok) {
+//       await login(email, password);
+//     }
+//     return false;
+//   }
 
-  return { token, login, logout, signup, update };
-}
+//   async function update(username, password, email, full_name) {
+//     const url = `${process.env.REACT_APP_TUNEWORLD_API_HOST}/api/accounts`;
+//     const response = await fetch(url, {
+//       method: "patch",
+//       body: JSON.stringify({
+//         username,
+//         password,
+//         email,
+//         full_name: full_name,
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     if (response.ok) {
+//       await login(username, password);
+//     }
+//     return false;
+//   }
 
-export const useUser = (token) => {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    if (!token) {
-      return;
-    }
-
-    async function getUser() {
-      const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/current_user`;
-      const response = await fetch(url, {
-        credentials: "include",
-      });
-      if (response.ok) {
-        const newUser = await response.json();
-        setUser(newUser);
-      }
-    }
-
-    getUser();
-  }, [token]);
-
-  return user;
-};
+//   return [token, login, logout, signup, update];
+// }

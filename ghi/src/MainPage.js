f23c755dc12fb9667d4e8bf90d934bd76a19b6a0
Alexa Randolph <alexarand7@gmail.com>
2023-02-22 19:06:33 -0500

    journal and auth

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,13 +1,11 @@
-import React, {useState, useEffect} from 'react'
-import TrendingMovies from './components/TrendingMovies'
+import React, { useState, useEffect } from "react";
+import TrendingMovies from "./components/TrendingMovies";
 
 export default function MainPage() {
-
-
   return (
     <>
-    <h1>MainPage</h1>
-    <TrendingMovies/>
+      <h1>MainPage</h1>
+      <TrendingMovies />
     </>
-  )
+  );
 }

4bb94bc0897e237528fa70e42607d674d23ec65e
Alexa Randolph <alexarand7@gmail.com>
2023-02-28 17:11:40 -0500

    saving progress on signup form

--- ghi/src/components/SignUp.js
+++ ghi/src/components/SignUp.js
@@ -43,77 +43,118 @@ export default function SignUp() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <div className="form-floating mb-3">
-          <input
-            onChange={handleFirstNameChange}
-            value={firstName}
-            placeholder="Enter your first name"
-            required
-            type="text"
-            name="firstName"
-            id="firstName"
-            className=""
-          />
-          <label htmlFor="firstName">First Name</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            onChange={handleLastNameChange}
-            value={lastName}
-            placeholder="Enter your last name"
-            required
-            type="text"
-            name="lastName"
-            id="lastName"
-            className=""
-          />
-          <label htmlFor="lastName">Last Name</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            onChange={handleUsernameChange}
-            value={username}
-            placeholder="Enter your desired username"
-            required
-            type="text"
-            name="username"
-            id="username"
-            className=""
-          />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            onChange={handleEmailChange}
-            value={email}
-            placeholder="Enter your email"
-            required
-            type="text"
-            name="email"
-            id="email"
-            className=""
-          />
-          <label htmlFor="email">E-Mail</label>
-        </div>
-        <div className="form-floating mb-3">
-          <input
-            onChange={handlePasswordChange}
-            value={password}
-            placeholder="Enter your desired password"
-            required
-            type="password"
-            name="password"
-            id="password"
-            className=""
-          />
-          <label htmlFor="password">Password</label>
-        </div>
-        <div>
-          <button type="submit">Sign up</button>
-        </div>
+    <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-blue-900 to-indigo-900">
+      <div className="container mx-auto flex items-center py-4 justify-center">
+        <div className="text-white font-Open Sans text-[50px]">Reel Review</div>
       </div>
-    </form>
+      <div className="w-full max-w-xs">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="form-floating mb-3">
+            <label
+              htmlFor="firstName"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              First Name
+            </label>
+            <input
+              onChange={handleFirstNameChange}
+              value={firstName}
+              placeholder="Enter your first name"
+              required
+              type="text"
+              name="firstName"
+              id="firstName"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <label
+              htmlFor="lastName"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Last Name
+            </label>
+            <input
+              onChange={handleLastNameChange}
+              value={lastName}
+              placeholder="Enter your last name"
+              required
+              type="text"
+              name="lastName"
+              id="lastName"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <label
+              htmlFor="username"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Username
+            </label>
+            <input
+              onChange={handleUsernameChange}
+              value={username}
+              placeholder="Enter your desired username"
+              required
+              type="text"
+              name="username"
+              id="username"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              E-Mail
+            </label>
+            <input
+              onChange={handleEmailChange}
+              value={email}
+              placeholder="Enter your email"
+              required
+              type="text"
+              name="email"
+              id="email"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="form-floating mb-3">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              onChange={handlePasswordChange}
+              value={password}
+              placeholder="********"
+              required
+              type="password"
+              name="password"
+              id="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="bg-sky-900 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
+        <p class="text-center text-gray-500 text-xs">
+          &copy;2023 Popcorn Posse LLC. All rights reserved.
+        </p>
+      </div>
+    </div>
   );
 }

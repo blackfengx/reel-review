67385310cf1678c06addf1f4793dabf8f93c9195
Alexa Randolph <alexarand7@gmail.com>
2023-03-02 15:18:01 -0500

    code cleanup

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Construct from "./Construct.js";
 import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 import MainPage from "./MainPage.js";
@@ -10,7 +9,6 @@ import SignUp from "./components/SignUp.js";
 import WelcomePage from "./components/WelcomePage.js";
 import MovieDetail from "./components/MovieDetail.js";
 import ReviewsForm from "./components/ReviewsForm.js";
-import Profile from "./components/Profile.js";
 import Nav from "./components/Nav.js";
 import ReviewList from "./components/ReviewList.js";
 
@@ -23,25 +21,20 @@ function GetToken() {
 function App() {
   const [error, setError] = useState(null);
 
-
   return (
     <div>
       <BrowserRouter>
         <AuthProvider>
-
           <GetToken />
           <Nav></Nav>
           <ErrorNotification error={error} />
-          {/* <Construct info={launch_info} /> */}
 
           <Routes>
-            {/* <Route path="/login" element={<Login />} /> */}
             <Route path="/" element={<MainPage />} />
             <Route path="/welcome" element={<WelcomePage />} />
             <Route path="/movie/detail/:id" element={<MovieDetail />} />
             <Route path="/reviews/list" element={<ReviewList />} />
             <Route path="/reviews/create/:id" element={<ReviewsForm />} />
-            {/* <Route path="/profile" element={<Profile />} /> */}
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/welcome" element={<WelcomePage />} />
